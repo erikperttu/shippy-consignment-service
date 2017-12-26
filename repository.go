0b2e7bc793466b4ee2aad5164a56c25b488c8274
Erik Perttu <erik.perttu@gmail.com>
2017-12-26 14:14:44 +0700

    fix repo import

--- repository.go
+++ repository.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	pb "github.com/erikperttu/shippy/consignment-service/proto/consignment"
+	pb "github.com/erikperttu/shippy-consignment-service/proto/consignment"
 	"gopkg.in/mgo.v2"
 )
 
